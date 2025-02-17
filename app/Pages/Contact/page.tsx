9cfe1b2230d89a55e183926a55d9ee4a0c47251e
Workplace <tayyabahmed7561@gmail.com>
2025-02-16 16:50:37 -0800

    done

--- app/Pages/Contact/page.tsx
+++ app/Pages/Contact/page.tsx
@@ -11,7 +11,7 @@ const IBM = IBM_Plex_Serif({
 });
 
 
-export default function page() {
+export default function ContactForm() {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
