a3904e1616fab343c8372aa0cfc2ed61f8ba582d
Workplace <tayyabahmed7561@gmail.com>
2025-05-05 13:51:28 -0700

    web

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar() {
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-transparent backdrop-blur-md text-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 ">
-            <div className="flex items-center justify-between border-b pb-6">
+            <div className="flex items-center justify-between border-b border-black pb-6">
               <Link href="/" className="-m-1.5 p-1.5">
                <h1 className={`${PoppinsFont.className} font-bold text-xl`}>Portfolio</h1>
               </Link>
